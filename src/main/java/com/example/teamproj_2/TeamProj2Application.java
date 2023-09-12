e60bc5c82e0774227a6ef82604c7c65c01b4624e
2dilution2 <2dilution2@gmail.com>
2023-09-12 15:52:42 +0900

    변경

---
+++ src/main/java/com/example/teamproj_2/TeamProj2Application.java
@@ -0,0 +1,13 @@
+package com.example.teamproj_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProj2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProj2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teamproj_2/TeamProj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamproj_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProj2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
